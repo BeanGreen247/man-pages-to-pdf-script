d21883ef7dc7ae54a1340050ff2b30c8ed182e54
Tomáš Mozdřeň <46648569+BeanGreen247@users.noreply.github.com>
2020-01-26 10:50:34 +0100

    small change

--- manpagetopdf.sh
+++ manpagetopdf.sh
@@ -12,6 +12,8 @@ man -k .|awk '{$0=$1};1' > manpagelist.csv
 
 # Here we use the data inside the manpagelist.csv file to generate the PDFs using the names sotred in the manpagelist.csv file
 filename="manpagelist.csv"
+# Reads till no trailing space, saves entry as the manpagename variable, executes the conversion to PDF file, 
+# moves to the next line and repeats till the end of the manpagelist.csv file
 while IFS=""; read -r manpagename; do
 	man -t $manpagename|ps2pdf - $manpagename.pdf
 done < $filename
